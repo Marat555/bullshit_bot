fc883de0ddf38e0f6bd3823d4bf85e8428c05eb0
Marat555 <maratradg98@mail.ru>
2025-04-22 16:25:38 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use sqlx::{Error as SqlxError, SqlitePool};
 use std::sync::Arc;
 use teloxide::{prelude::*, types::Message};
 
+pub type AppErr = Box<dyn std::error::Error + Send + Sync>;
+pub type HandlerResult = Result<(), AppErr>;
+
+static REPLY_CHANCE: f64 = 0.25;
+
 async fn init_db(pool: &SqlitePool) -> Result<(), SqlxError> {
     sqlx::query(
         "CREATE TABLE IF NOT EXISTS responces (
@@ -45,33 +50,28 @@ async fn main() {
 
     let bot = Bot::new(token);
 
-    let reply_chance = 0.25;
-
-    let handler = Update::filter_message().endpoint({
-        let pool = Arc::clone(&pool);
-        move |msg: Message, bot: Bot| {
-            let pool = Arc::clone(&pool);
-            async move {
-                let chat_id = msg.chat.id;
-
-                if msg.chat.is_group() || msg.chat.is_supergroup() {
-                    let random_number: f64 = rand::thread_rng().r#gen();
-
-                    if random_number < reply_chance {
-                        if let Some(response) = get_random_response(&pool).await {
-                            bot.send_message(chat_id, response).await?;
-                        }
-                    }
-                }
-
-                respond(())
-            }
-        }
-    });
+    let handler = Update::filter_message().endpoint(start_handler);
 
     Dispatcher::builder(bot, handler)
         .enable_ctrlc_handler()
+        .dependencies(dptree::deps![pool.clone()])
         .build()
         .dispatch()
         .await;
+}
+
+async fn start_handler(msg: Message, bot: Bot, pool: Arc<SqlitePool>) -> HandlerResult {
+    let chat_id = msg.chat.id;
+
+    if msg.chat.is_group() || msg.chat.is_supergroup() {
+        let random_number: f64 = rand::thread_rng().r#gen();
+
+        if random_number < REPLY_CHANCE {
+            if let Some(response) = get_random_response(&pool).await {
+                bot.send_message(chat_id, response).await?;
+            }
+        }
+    }
+
+    Ok(())
 }
